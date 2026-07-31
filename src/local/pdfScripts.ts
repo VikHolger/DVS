56fa94258fcf781f2c8d841fb6b27039c0597320
Viktor <kolo.eri@gmail.com>
2026-07-31 16:42:40 +0200

    v 0.2.15 - Maybye works now?

--- src/local/pdfScripts.ts
+++ src/local/pdfScripts.ts
@@ -8,8 +8,8 @@ const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
 const loadFonts = async (): Promise<Font> => {
   try {
     // Use absolute URLs for Next.js public folder
-    const fontUrl1 = '${basePath}/fonts/Raleway-SemiBoldItalic.ttf';
-    const fontUrl2 = '${basePath}/fonts/FiraCode-Regular.ttf';
+    const fontUrl1 = `${basePath}/fonts/Raleway-SemiBoldItalic.ttf`;
+    const fontUrl2 = `${basePath}/fonts/FiraCode-Regular.ttf`;
     
     console.log('Loading fonts from:', fontUrl1, fontUrl2);
     
